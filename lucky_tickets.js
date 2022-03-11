be83e737e03735bc186dc3983259da4e200f87a0
Sasha <61920568+SashaJozwiak@users.noreply.github.com>
2022-03-11 06:31:07 +0300

    fix: add let fot i

--- lucky_tickets.js
+++ lucky_tickets.js
@@ -2,13 +2,13 @@
 
 let result = 0;
 
-for (i = 0; i <= 999999; i++) {
+for (let i = 0; i <= 999999; i++) {
     let arr = String(i).padStart(6, '0').split('');
 
-    let sum1 = Number(arr[0]) + Number(arr[1]) + Number(arr[2])
-    let sum2 = Number(arr[3]) + Number(arr[4]) + Number(arr[5])
+    let sumFirst = Number(arr[0]) + Number(arr[1]) + Number(arr[2])
+    let sumSecond = Number(arr[3]) + Number(arr[4]) + Number(arr[5])
 
-    if (sum1 === sum2) result++;
+    if (sumFirst === sumSecond) result++;
 }
 
 console.log(result) // 55 252
@@ -16,7 +16,7 @@ console.log(result) // 55 252
 //shortly
 
 let result = 0;
-for (i = 0; i <= 999999; i++) {
+for (let i = 0; i <= 999999; i++) {
     let arr = ("" + i).padStart(6, '0').split('');
     (+(arr[0]) + +(arr[1]) + +(arr[2])) === +(arr[3]) + +(arr[4]) + +(arr[5]) ? result++ : false;
 } 
