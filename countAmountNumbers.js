5ab5ba0d08aace5438b30d26856ef06621a34b35
Sasha <61920568+SashaJozwiak@users.noreply.github.com>
2023-02-15 15:08:01 +0300

    feat: check amount numbers solution

---
+++ countAmountNumbers.js
@@ -0,0 +1,12 @@
+let num = 3132434213645;
+
+function luckyNumbers(num) {
+  let arr = String(num).split("");
+  let obj = {};
+
+  arr.forEach((item) => {
+    obj[item] ? obj[item]++ : (obj[item] = 1);
+  });
+}
+
+console.log(luckyNumbers(num));
